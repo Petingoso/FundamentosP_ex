18f305d792f1ef37bc17a6f6b88f461149532fd2
Petingoso <petingavasco@protonmail.com>
2024-09-21 23:18:33 +0100

    added nix-direnv stuff

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d9d07251f12399413e6d33d5875a6f1994ef75a7.tar.gz") {};
+in
+  pkgs.mkShellNoCC {
+    packages = [
+      (
+        pkgs.python3
+      )
+    ];
+  }
