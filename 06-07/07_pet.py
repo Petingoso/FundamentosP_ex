92188604a51da031c3319e20f9f30a1ce416c80e
Petingoso <petingavasco@protonmail.com>
2024-10-01 09:49:42 +0100

    07/07_pet tweaked
    07/07_pet fixed

--- 06-07/07_pet.py
+++ 06-07/07_pet.py
@@ -1,17 +1,18 @@
-def amigas(t):
+def amigas(t1,t2):
+    if len(t1) != len(t2):
+        return False 
+
     amount = 0
-    for i in range(len(t[0])):
-        print(t[0][i], t[1][i])
-        if t[0][i] != t[1][i]:
+    for i in range(len(t1)):
+        if t1[i] != t2[i]:
              amount+=1
 
-
     if amount: # pelo menos 1 diferença
-        return (( amount/len(t[0]) )* 100) < 10 # tamanho / numero vezes(>0), * 100 para percentagem
+        # tamanho / numero vezes(>0), * 100 para percentagem
+        return (( amount/len(t1) )* 100) < 10     
     else:
-        # print(len(t[0]))
         return True # caso 0 difs
 
-print(amigas(('amigas','amigas')))
-print(amigas(('amigas','asigos')))
-print(amigas(('amigassssss','amigosssssss')))
+print(amigas('amigas','amigas'))
+print(amigas('amigas','asigos'))
+print(amigas('amigassssss','amigosssssss'))
