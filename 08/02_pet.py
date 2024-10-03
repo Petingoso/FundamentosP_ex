90d39731655d7488ae321b85c30fb7f9ea45e2ea
Petingoso <petingavasco@protonmail.com>
2024-10-03 08:57:41 +0100

    08/02 tweaked

--- 08/02_pet.py
+++ 08/02_pet.py
@@ -1,9 +1,21 @@
-def remove_multiplos(w,x):
-    i = 0
-    while i<len(w):
+# def remove_multiplos1(w,x):
+#     i = 0
+#     while i<len(w):
+#         if (w[i]%x==0):
+#             w.remove(w[i])
+#             i-=1
+#         i+=1
+#     return w 
+# print(remove_multiplos1([2, 3, 5, 9, 12, 33, 34, 45], 3))
+
+def remove_multiplos1(w,x):
+    for i in range(len(w)-1,-1,-1):
         if (w[i]%x==0):
             w.remove(w[i])
-            i-=1
-        i+=1
     return w 
-print(remove_multiplos([2, 3, 5, 9, 12, 33, 34, 45], 3))
+
+print(remove_multiplos1([2, 3, 5, 9, 12, 33, 34, 45], 3))
+
+def remove_multiplos2(w,x):
+    return [i for i in w if i%x]
+print(remove_multiplos2([2, 3, 5, 9, 12, 33, 34, 45], 3))
